09c6e8d9f5c5155bb8a288b6aedabd49109a52b7
Ben168-stack <84187925+Ben168-stack@users.noreply.github.com>
2021-09-09 20:44:17 +0800

    Update Employee.py

--- Practical 2/Practical 2 Q3/Employee.py
+++ Practical 2/Practical 2 Q3/Employee.py
@@ -7,3 +7,11 @@ class Employee:
     def __str__(self):
         return f'Name: {self.__name}, ID: {self.__id}, Department: {self.__department}, ' \
                f'Title: {self.__title}'
+    def edit_name(self,name):
+        self.__name = name
+    def edit_id(self,id):
+        self.__id = id
+    def edit_department(self,department):
+        self.__department= department
+    def edit_title(self,title):
+        self.__title = title
