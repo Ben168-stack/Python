1d9d110e72e8ae47b04e5d33abe6c19bfeb193a1
Ben168-stack <84187925+Ben168-stack@users.noreply.github.com>
2021-09-10 13:48:37 +0800

    Create Monster.py

---
+++ Practical 4/Monster.py
@@ -0,0 +1,13 @@
+class Monster:
+    def __init__(self,name,health,attack,defence):
+        self.__name = name
+        self.__health = health
+        self.__attack = attack
+        self.__defence = defence
+
+    def __str__(self):
+         return f"{self.__name} is a monster"
+
+
+
+print(Monster("Boi",100,100,100))
