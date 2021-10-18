f89a9d62fd0dbe8149b3f2ee1cf308d1bfa7662f
Benji <84187925+Ben168-stack@users.noreply.github.com>
2021-10-18 22:42:50 +0800

    Create Q1 Customer.py

---
+++ App Development/Classes & Objects/Q1 Customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    def __init__(self,name,email,mobile_number):
+        self.__name = name
+        self.__email = email
+        self.__mobile_number = mobile_number
+    def get_customer_info(self):
+        return f'Name {self.__name}, Email:{self.__email}, Mobile Number: {self.__mobile_number}'
+
+
+Customer = Customer('John','john@nyp.edu.sg',92345678)
+print(Customer.get_customer_info())
